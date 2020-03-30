54a1fb127dc2223c5e4db738b4142c1acf0c3fd5
juliennicaise <58558170+juliennicaise@users.noreply.github.com>
2020-03-30 17:44:29 +0200

    atelier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Avatar from './avatar.js';
+import BartData from './bart.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        
+        <Avatar firstName='Homer' image="https://vignette.wikia.nocookie.net/simpsons/images/3/33/Homer.png/revision/latest/scale-to-width-down/340?cb=20110703093455&path-prefix=fr" lastName='SIMPSON' />
+        <Avatar firstName='Marge' image="https://vignette.wikia.nocookie.net/simpsons/images/a/a1/Marge_Simpsons.png/revision/latest/scale-to-width-down/340?cb=20130817213926&path-prefix=fr" lastName='SIMPSON' />
+        <Avatar firstName='Bart' image="https://img.20mn.fr/MB0FPhiQS5i8gBmTUKVRJg/830x532_rip-bart-temporairement.jpg" lastName='SIMPSON' />
+        <Avatar {...BartData} />
     </div>
   );
 }
